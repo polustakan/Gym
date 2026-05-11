db32dafa041632608e8811b81908866b88a66871
Ilja <kirillfromukraine@gmail.com>
2026-05-11 21:55:09 +0300

    fleWriter and fileReader

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,4 +1,7 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class Visitor {
@@ -13,8 +16,19 @@ public class Visitor {
         LocalDate end = now.plusMonths(months);
         System.out.println(end);
     }
+    public  void dataWriter(){
+        try {
+            FileWriter fw = new FileWriter("database.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public void dataReader (){
-        FileReader fr = new FileReader(database.txt);
+        try {
+            FileReader fr = new FileReader("database.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
